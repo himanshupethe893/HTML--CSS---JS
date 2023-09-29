3d48e0d2e7853563af679dc5bf3b7beeb28b222a
Hitman003 <himanshupethe08092003@gmail.com>
2023-09-30 02:48:56 +0530

    commit

---
+++ Traffic_light/T.js
@@ -0,0 +1,35 @@
+function stopb(){
+    document.getElementById("stop").style.backgroundColor="red";
+    document.getElementById("wait").style.backgroundColor="gray";
+    document.getElementById("Go").style.backgroundColor="gray";
+    document.getElementById("sto").style.backgroundColor="red";
+    document.getElementById("wai").style.backgroundColor="gray";
+    document.getElementById("gon").style.backgroundColor="gray";
+}
+
+function waitb(){
+    document.getElementById("stop").style.backgroundColor="gray";
+    document.getElementById("wait").style.backgroundColor="yellow";
+    document.getElementById("Go").style.backgroundColor="gray";
+    document.getElementById("sto").style.backgroundColor="gray";
+    document.getElementById("wai").style.backgroundColor="yellow";
+    document.getElementById("gon").style.backgroundColor="gray";
+}
+
+function gob(){
+    document.getElementById("stop").style.backgroundColor="gray";
+    document.getElementById("wait").style.backgroundColor="gray";
+    document.getElementById("Go").style.backgroundColor="green";
+    document.getElementById("sto").style.backgroundColor="gray";
+    document.getElementById("wai").style.backgroundColor="gray";
+    document.getElementById("gon").style.backgroundColor="green";
+}
+
+function rtb(){
+    document.getElementById("stop").style.backgroundColor="gray";
+    document.getElementById("wait").style.backgroundColor="gray";
+    document.getElementById("Go").style.backgroundColor="gray";
+    document.getElementById("sto").style.backgroundColor="gray";
+    document.getElementById("wai").style.backgroundColor="gray";
+    document.getElementById("gon").style.backgroundColor="gray";
+}
