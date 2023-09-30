d8534a1c1944681ea1c758ce70fff2e5ce7d2815
Hitman003 <himanshupethe08092003@gmail.com>
2023-10-01 02:12:49 +0530

    Initial commit

---
+++ color_changer/c.js
@@ -0,0 +1,23 @@
+function su1()
+{
+    document.getElementById("bod1").style.backgroundImage="linear-gradient(to bottom, orange, white, green)";
+    document.getElementById("r1").innerHTML="Indian Flag"
+}
+
+function su2()
+{
+    document.getElementById("bod1").style.backgroundImage="linear-gradient(to bottom, orange, red)";
+    document.getElementById("r1").innerHTML="Indian Flag"
+}
+
+function su3()
+{
+    document.getElementById("bod1").style.backgroundImage="linear-gradient(to bottom, aqua, darkblue)";
+    document.getElementById("r1").innerHTML="Indian Flag"
+}
+
+function su4()
+{
+    document.getElementById("bod1").style.backgroundImage="url('cloud.jpg')";
+    document.getElementById("r1").innerHTML="Indian Flag"
+}
