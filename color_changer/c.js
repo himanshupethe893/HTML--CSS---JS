3ccb48d8b1303e6fd546ae8b822bb47691559dd8
Hitman003 <himanshupethe08092003@gmail.com>
2023-10-01 03:31:31 +0530

    commit

--- color_changer/c.js
+++ color_changer/c.js
@@ -1,23 +1,23 @@
-function su1()
-{
-    document.getElementById("bod1").style.backgroundImage="linear-gradient(to bottom, orange, white, green)";
-    document.getElementById("r1").innerHTML="Indian Flag"
+function su1() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, orange, white, green)";
+    document.getElementById("r1").innerHTML = "Indian Flag";
+    document.body.style.backgroundImage="linear-gradient(to bottom,  orange, white, green)";
 }
 
-function su2()
-{
-    document.getElementById("bod1").style.backgroundImage="linear-gradient(to bottom, orange, red)";
-    document.getElementById("r1").innerHTML="Indian Flag"
+function su2() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, orange, red)";
+    document.getElementById("r1").innerHTML = "Fire";
+    document.body.style.backgroundImage="linear-gradient(to bottom, orange, red)";
 }
 
-function su3()
-{
-    document.getElementById("bod1").style.backgroundImage="linear-gradient(to bottom, aqua, darkblue)";
-    document.getElementById("r1").innerHTML="Indian Flag"
+function su3() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, aqua, darkblue)";
+    document.getElementById("r1").innerHTML = "Water";
+    document.body.style.backgroundImage="linear-gradient(to bottom, aqua, darkblue)";
 }
 
-function su4()
-{
-    document.getElementById("bod1").style.backgroundImage="url('cloud.jpg')";
-    document.getElementById("r1").innerHTML="Indian Flag"
+function su4() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, white, gray)";
+    document.getElementById("r1").innerHTML = "Clouds";
+    document.body.style.backgroundImage="linear-gradient(to bottom, white, gray)";
 }
